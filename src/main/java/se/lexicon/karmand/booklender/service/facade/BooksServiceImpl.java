54bf1ccb542cdbcdc120d44305716034d5b3a8d5
KarmandAziz <Karmand94@gmail.com>
2022-01-14 13:57:55 +0100

    BookServiceImpl added

--- src/main/java/se/lexicon/karmand/booklender/service/facade/BooksServiceImpl.java
+++ src/main/java/se/lexicon/karmand/booklender/service/facade/BooksServiceImpl.java
@@ -7,58 +7,71 @@ import se.lexicon.karmand.booklender.data.dao.BooksDAO;
 import se.lexicon.karmand.booklender.model.dto.BooksDTO;
 import se.lexicon.karmand.booklender.model.forms.BooksForm;
 import se.lexicon.karmand.booklender.service.DTOService;
+import se.lexicon.karmand.booklender.service.entity.BooksEntityService;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
 public class BooksServiceImpl implements BooksService{
 
-    private BooksDAO booksDAO;
+    private BooksEntityService booksEntityService;
     private DTOService dtoService;
 
     @Autowired
-    public BooksServiceImpl(BooksDAO booksDAO, DTOService dtoService) {
-        this.booksDAO = booksDAO;
+    public BooksServiceImpl(BooksEntityService booksEntityService, DTOService dtoService) {
+        this.booksEntityService = booksEntityService;
         this.dtoService = dtoService;
     }
 
+
     @Override
     public BooksDTO create(BooksForm booksForm) {
-        return null;
+        return dtoService.toBooksDTO(booksEntityService.create(booksForm));
     }
 
     @Override
     public List<BooksDTO> findAll() {
-        return null;
+        return booksEntityService.findAll().stream()
+                .map(dtoService::toBooksDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public BooksDTO update(String id, BooksForm booksForm) {
-        return null;
+        return dtoService.toBooksDTO(booksEntityService.update(id, booksForm));
     }
 
     @Override
     public void delete(String id) {
+        booksEntityService.delete(id);
 
     }
 
     @Override
     public BooksDTO findById(String bookId) {
-        return null;
+        return dtoService.toBooksDTO(booksEntityService.findById(bookId));
     }
 
     @Override
     public List<BooksDTO> findByTitle(String title) {
-        return null;
+        return booksEntityService.findByTitle(title).stream()
+                .map(dtoService::toBooksDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<BooksDTO> findByAvailable(boolean available) {
-        return null;
+        return booksEntityService.findByAvailableStatus(available).stream()
+                .map(dtoService::toBooksDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<BooksDTO> findByReserved(boolean reserved) {
-        return null;
+        return booksEntityService.findByReservedStatus(reserved)
+                .stream().map(dtoService::toBooksDTO)
+                .collect(Collectors.toList());
     }
 }
