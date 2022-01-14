2b1db37d4da2090ba4082883133ebc109549cb8f
KarmandAziz <Karmand94@gmail.com>
2022-01-14 08:32:41 +0100

    findByEmail query added

--- src/main/java/se/lexicon/karmand/booklender/data/dao/LibraryUserDAO.java
+++ src/main/java/se/lexicon/karmand/booklender/data/dao/LibraryUserDAO.java
@@ -1,7 +1,15 @@
 package se.lexicon.karmand.booklender.data.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import se.lexicon.karmand.booklender.model.entity.LibraryUser;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LibraryUserDAO extends JpaRepository<LibraryUser, String> {
+
+    @Query("SELECT e FROM LibraryUser e WHERE UPPER(e.email) = UPPER(:email)")
+    Optional<LibraryUser> findByEmail(@Param("email") String email);
 }
